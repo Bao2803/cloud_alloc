82f3e744b3d21834201f78f5977087239ce034a9
Bao Quoc Phan <phanquocbao2803@gmail.com>
2024-04-30 00:01:51 -0400

    Spring Boot Init

---
+++ src/main/java/westwood222/cloud_alloc/CloudAllocApplication.java
@@ -0,0 +1,13 @@
+package westwood222.cloud_alloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAllocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAllocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/westwood222/cloud_alloc/CloudAllocApplicationTests.java
@@ -0,0 +1,13 @@
+package westwood222.cloud_alloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAllocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
